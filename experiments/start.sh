57969e498ce1fa32a627852018d44baf76122bab
Nik <nikvaes@gmail.com>
2019-05-12 18:16:16 +0200

    rename now complete

--- experiments/start.sh
+++ experiments/start.sh
@@ -3,9 +3,9 @@
 set -e
 
 tmux new-session -d -s exp
-tmux send -t project_experiments "source venv/bin/activate" ENTER
-tmux send -t project_experiments "export PYTHONPATH=${PYTHONPATH}:../" ENTER
-tmux send -t project_experiments "python3 run_experiments.py queue/ results/" ENTER
+tmux send -t exp "source venv/bin/activate" ENTER
+tmux send -t exp "export PYTHONPATH=${PYTHONPATH}:../" ENTER
+tmux send -t exp "python3 run_experiments.py queue/ results/" ENTER
 
 tmux new-session -d -s tensorboard
 tmux send -t tensorboard "source .env/bin/activate" ENTER
