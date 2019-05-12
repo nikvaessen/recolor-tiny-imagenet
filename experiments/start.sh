4106fbb70125cd2eb2b539de4089d0a5feff353f
Nik <nikvaes@gmail.com>
2019-05-12 17:30:41 +0200

    update start script

--- experiments/start.sh
+++ experiments/start.sh
@@ -4,12 +4,11 @@ set -e
 
 tmux new-session -d -s project_experiments
 tmux send -t project_experiments "source .env/bin/activate" ENTER
-tmux send -t project_experiments "export PYTHONPATH=${PYTHONPATH}:../$(pwd)" ENTER
+tmux send -t project_experiments "export PYTHONPATH=${PYTHONPATH}:../" ENTER
 tmux send -t project_experiments "python3 run_experiments.py queue/ results/" ENTER
 
 tmux new-session -d -s tensorboard
 tmux send -t tensorboard "source .env/bin/activate" ENTER
-tmux send -t tensorboard "export PYTHONPATH=${PYTHONPATH}:../$(pwd)" ENTER
 tmux send -t tensorboard "tensorboard --logdir results/" ENTER
 
 tmux new-session -d -s sync
