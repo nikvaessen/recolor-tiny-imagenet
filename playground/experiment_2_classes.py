5269a9c6de1326d247211948fb5dd99e59b7749d
Nik <nikvaes@gmail.com>
2019-05-16 19:03:06 +0200

    save correct progress images during 2 class experiment

--- playground/experiment_2_classes.py
+++ playground/experiment_2_classes.py
@@ -2,7 +2,7 @@ import os
 
 import numpy as np
 
-from recolor.constants import load_pickled_data
+from recolor.constants import load_pickled_data, dog_val, fish_val
 
 
 prob_dir = os.path.join("..", "probabilities")
@@ -18,3 +18,13 @@ print(weights_dog, weights_dog.shape)
 
 from recolor.constants import weights
 print(weights, weights.shape)
+
+
+print(dog_val)
+print(fish_val)
+
+for d_path in [] + dog_val + fish_val:
+    # print(d_path)
+    tag = os.path.splitext(os.path.split(d_path)[1])[0] + ".JPEG"
+    path = os.path.join("..", "data", "tiny-imagenet-200", "val", "images", tag)
+    print(path)
