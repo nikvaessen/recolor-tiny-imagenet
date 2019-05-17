c3692a3aae22ea614b3922b1796e51f5eb93ca99
Nik <nikvaes@gmail.com>
2019-05-17 15:49:51 +0200

    remove 2 layers from the network :)

--- experiments/start.sh
+++ experiments/start.sh
@@ -10,7 +10,7 @@ tmux new-session -d -s exp
 tmux send -t exp "source ../venv/bin/activate" ENTER
 tmux send -t exp "export PYTHONPATH=${PYTHONPATH}:../" ENTER
 tmux send -t exp "python3 run_experiments.py queue/ results/" ENTER
-echo "please manually start the queue"
+echo "this branch is running a smaller network with 2 layers removed :)"
 
 tmux new-session -d -s tensorboard
 tmux send -t tensorboard "source .env/bin/activate" ENTER
