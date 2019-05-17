ef7ed77bf9e539bccb828c3a24ad4417a260d5ef
Nik <nikvaes@gmail.com>
2019-05-17 17:20:40 +0200

    script to start vgg

---
+++ Classification/start_vgg.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+if tmux info &> /dev/null; then
+  tmux kill-server
+fi
+
+tmux new-session -d -s tensorboard
+tmux send -t tensorboard "source .env/bin/activate" ENTER
+tmux send -t tensorboard "tensorboard --logdir results/" ENTER
+
+
+source ../venv/bin/activate
+export PYTHONPATH=${PYTHONPATH}:../
+
+python3 image_classifier.py
+
+sudo shutdown now
+
