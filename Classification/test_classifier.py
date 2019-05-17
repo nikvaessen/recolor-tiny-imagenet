f7f6e6d5e65c677f6f8195b32f7392d7078c3e64
Nik <nikvaes@gmail.com>
2019-05-17 15:47:16 +0200

    WIP :)

--- Classification/test_classifier.py
+++ Classification/test_classifier.py
@@ -12,25 +12,38 @@ def main():
         test_file_paths = pickle.load(f)
         print(test_file_paths[0])
 
-    mode = 'colour'
-    dim_in = (64, 64, 3)
-    shuffle = True
-    batch_size = 1
-    n_classes = 30
-    dim_out = (n_classes)
+    with open('id_label.pickle', 'rb') as f:
+        labels = pickle.load(f)
+        print(labels)
 
-    training_generator = data_utils.DataGenerator(test_file_paths, batch_size,
-                                                  dim_in, dim_out, shuffle,
-                                                  mode, 'validation')
+    with open('id_name.pickle', 'rb') as f:
+        name_to_label = pickle.load(f)
+        print(name_to_label)
 
-    num_samples = training_generator.__len__()
+    with open('label_id.pickle', 'rb') as f:
+        lable_id = pickle.load(f)
+        print(lable_id)
 
-    sample = training_generator[0]
+    #
+    # mode = 'colour'
+    # dim_in = (64, 64, 3)
+    # shuffle = True
+    # batch_size = 1
+    # n_classes = 30
+    # dim_out = (n_classes)
+    #
+    # training_generator = data_utils.DataGenerator(test_file_paths, batch_size,
+    #                                               dim_in, dim_out, shuffle,
+    #                                               mode, 'validation')
+    #
+    # num_samples = training_generator.__len__()
+    #
+    # sample = training_generator[0]
+    #
+    # print(sample)
 
-    print(sample)
-
-    model = keras.models.load_model(model_path)
-    model = keras.models.Model()
+    # model = keras.models.load_model(model_path)
+    # model = keras.models.Model()
 
 
 
